76fbe050d413001c97b6ae186b5e8cd374816a7a
Shubham Dubey <dubeyshubham154@gmail.com>
2023-10-15 07:56:43 +0000

    datatypes in javascript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 )  // we are using nodejs, not browser.
+
+console.log(33+3);
+console.log("Shubham");
+
+// Data Types
+
+// numbers => 2 to the power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+//undefined => 
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
+
